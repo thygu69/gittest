410612a152f2c82b81bc7de21ac9cc27e39a75f4
thygu69 <thygu69@gmail.com>
2021-10-05 14:02:12 +1030

    third commit

---
+++ src/test/java/sampleTest1/SampleTest1.java
@@ -0,0 +1,30 @@
+package sampleTest1;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest1 {
+
+	@Test
+	public void loginTest () throws IOException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd=new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com/");
+		
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		
+		///Users/e11/Documents/online/workspace/batch181/C2_WebdriverTest/screenshot
+		//FileUtils //appache //1.0, 2.0
+		FileHandler.copy(src, new File("C:\\Users\\User\\workspace\\gitTest\\screenshot\\google.png"));
+	}
+}

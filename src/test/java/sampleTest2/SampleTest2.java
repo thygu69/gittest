7e8093a10c2c9ba44958009898b0b7b08c910b83
thygu69 <thygu69@gmail.com>
2021-10-11 22:46:39 +1030

    jenkin

--- src/test/java/sampleTest2/SampleTest2.java
+++ src/test/java/sampleTest2/SampleTest2.java
@@ -1,4 +1,4 @@
-package sampleTest1;
+package sampleTest2;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,22 +12,23 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class SampleTest1 {
+public class SampleTest2 {
 
 	@Test
 	public void loginTest () throws IOException, InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver wd=new ChromeDriver();
 		wd.manage().window().maximize();
-		wd.get("https://www.google.com/");
+		wd.get("https://www.bing.com/");
 		
 		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
 		
 		///Users/e11/Documents/online/workspace/batch181/C2_WebdriverTest/screenshot
 		//FileUtils //appache //1.0, 2.0
-		FileHandler.copy(src, new File("C:\\Users\\User\\workspace\\gitTest\\screenshot\\google.jpg"));
-		
-		int i=1/0;
+		FileHandler.copy(src, new File("C:\\Users\\User\\workspace\\gitTest\\screenshot\\bing.jpg"));
+		Thread.sleep(2000);
 		wd.quit();
 	}
 }
+
+
